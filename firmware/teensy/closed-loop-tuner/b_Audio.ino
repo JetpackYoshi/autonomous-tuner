8931617cbb85d24a7a423139c01d0e735f3d0359
DESKTOP-H7TLVS7\yoshi <yg353@drexel.edu>
2019-02-17 12:05:46 -0500

    Added in function along with a switch case to easily move between strings.

--- firmware/teensy/closed-loop-tuner/b_Audio.ino
+++ firmware/teensy/closed-loop-tuner/b_Audio.ino
@@ -1,9 +1,18 @@
-#define NOTE_E5 659.3
-#define NOTE_A4 440.0
-#define NOTE_D4 293.7
-#define NOTE_G3 196.0
+#include <PID_v1.h>
+//#include "myEnums.h"
+//#define NOTE_E5 659.3
+//#define NOTE_A4 440.0
+//#define NOTE_D4 293.7
+//#define NOTE_G3 196.0
 
-float FREQ_TARGET = NOTE_A4;
+//enum TuningTargets{STRING_G3, STRING_D4, STRING_A4, STRING_E5};
+TuningTargets tuningTarget = STRING_D4;
+
+double Setpoint, Input, Output;
+double p_Kp=2, p_Ki=5, p_Kd=1;
+PID tuningPID(&Input, &Output, &Setpoint, p_Kp, p_Ki, p_Kd, DIRECT);
+
+float FREQ_TARGET;
 float focusBand = 3;
 
 AudioInputAnalog              adc1; //adc(A2);
@@ -12,29 +21,67 @@ AudioConnection               patchCord1(adc1,0, notefreq1,0);
 
 long lastSampleTime = 0;
 
+void setTuningTarget(TuningTargets target){
+  switch(target){
+    case STRING_G3: {
+      FREQ_TARGET = 196.0;
+      break;
+    }
+    case STRING_D4: {
+      FREQ_TARGET = 293.7;
+      break;
+    }
+    case STRING_A4: {
+      FREQ_TARGET = 440.0;
+      break;
+    }
+    case STRING_E5: {
+      FREQ_TARGET = 659.3;
+      break;
+    }
+  }
+}
+
 void audioSetup(){
   AudioMemory(30);
   notefreq1.begin(.15);
+  tuningPID.SetMode(AUTOMATIC);
+  tuningPID.SetOutputLimits(-speedLimit,speedLimit);
+  setTuningTarget(STRING_E5);
 }
 void detectPitch(){
   //Serial.println("TEST");
   if (notefreq1.available()) {
     
-    noInterrupts();
     lastSampleTime = millis();
     note = notefreq1.read();
     prob = notefreq1.probability();
     //Serial.printf("Note: %3.2f | Probability: %.2f\n", note, prob);
-
     
-    if (note>400 && note <500){
+    if (note>FREQ_TARGET-50 && note <FREQ_TARGET+50){
       Serial.printf("%3.2f\n", note);
-      stepperSpeed = map(note, FREQ_TARGET-focusBand, FREQ_TARGET+focusBand, -300, 300);
+      noInterrupts();
+      stepperSpeed = map(note, FREQ_TARGET-focusBand, FREQ_TARGET+focusBand, -speedLimit, speedLimit);
+      interrupts();
     }
-    interrupts();
+    
   }
 
   if ((millis()-lastSampleTime) > 250){
    stepperSpeed = 0;
+  }
+}
+
+void tuneString(){
+  if (notefreq1.available()) {
+    if (note>400 && note <500){
+      Serial.printf("%3.2f\n", note);
+      noInterrupts();
+      stepperSpeed = map(note, FREQ_TARGET-focusBand, FREQ_TARGET+focusBand, -300, 300);
+      interrupts();
+    }
+    if ((millis()-lastSampleTime) > 250){
+      stepperSpeed = 0;
+    }
   }
 }
--- firmware/teensy/closed-loop-tuner/closed-loop-tuner.ino
+++ firmware/teensy/closed-loop-tuner/closed-loop-tuner.ino
@@ -1,6 +1,7 @@
 #include <Audio.h>
 #include <SPI.h>
 #include <Wire.h>
+enum TuningTargets{STRING_G3, STRING_D4, STRING_A4, STRING_E5};
 
 #define kSerialSpeed 115200
 
